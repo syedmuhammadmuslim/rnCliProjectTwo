59549833b0bc3d8abdd1851f1708cf32171bc236
syedmuhammadmuslim <syedmuhammadmuslim@gmail.com>
2023-04-03 14:08:48 +0500

    Second Commit

---
+++ App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View, StyleSheet, Button, Alert} from 'react-native';
+
+const App = () => {
+  const buttonPressed = msg => {
+    alert('Hi from ' + msg);
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+      <View style={{flexDirection: 'row'}}>
+        <View style={{padding: 10}}>
+          <Button
+            title="Click Me!"
+            onPress={buttonPressed.bind(this, 'Button 1')}
+          />
+        </View>
+        <View style={{padding: 10}}>
+          <Button
+            title="Click Me!"
+            onPress={buttonPressed.bind(this, 'Button 2')}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    fontSize: 30,
+    color: 'ffffff',
+  },
+  text: {
+    fontSize: 30,
+    color: '#654321',
+  },
+});
+
+export default App;
